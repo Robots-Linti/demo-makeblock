b8ee8aaa91466d779ab37233f6d05d4ac734fa61
ecrudele <ecrudele@users.noreply.github.com>
2018-04-18 14:59:33 -0300

    agregado programa recorrido polígono

--- firmware_demo_campus_party/firmware_demo_campus_party.ino
+++ firmware_demo_campus_party/firmware_demo_campus_party.ino
@@ -95,8 +95,12 @@ double angle_rad = PI/180.0;
 double angle_deg = 180.0/PI;
 
 /* VARIABLES NUESTRAS */
-void setSpeed(double number1);
+void setRotation(double speed);
+void setSpeed(double speed);
+void rotateLeft(double degrees);
+MeGyro gyro_1;
 MeUltrasonicSensor ultrasonic_7(7);
+double turningSpeed;
 double normalSpeed;
 double umbral;
 /* FIN VARIABLES NUESTRAS */
@@ -111,33 +115,59 @@ void setup(){
     Serial.begin(9600);
     attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
     attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
+    gyro_1.begin();
+    turningSpeed = 50;
     normalSpeed = 255;
     umbral = 25;
 /* FIN INICIALIZACIÓN VARIABLES NUESTRAS */
 }
 
 /* FUNCIONES NUESTRAS */
-void setSpeed(double number1)
+void setRotation(double speed)
 {
-    Encoder_1.setTarPWM((0) - (number1));
-    Encoder_2.setTarPWM(number1);
+    Encoder_1.setTarPWM(speed);
+    Encoder_2.setTarPWM(speed);
+}
+
+void setSpeed(double speed)
+{
+    Encoder_1.setTarPWM((0) - (speed));
+    Encoder_2.setTarPWM(speed);
+}
+
+void rotateLeft(double degrees)
+{
+    double endValue = (fmod(((gyro_1.getAngle(3)) + (180)) + (degrees),360)) - (180);
+    setRotation(turningSpeed);
+    if((gyro_1.getAngle(3)) > (endValue)){
+        while(!(( 0 ) > (gyro_1.getAngle(3))))
+        {
+            _loop();
+        }
+    }
+    while(!((gyro_1.getAngle(3)) > (endValue)))
+    {
+        _loop();
+    }
+    setSpeed(0);
 }
 
 int leerOpcion() {
-
-    // DEFINIR metodo de entrada (bt, serial, etc.)
-    // y tomar la opcion elegida de ahi
-
+/* la lectura de la elección se realiza mediante el sensor de distancia;
+apoyando la mano a cierta distancia (en intervalos de 10cm) se elige una opción */
     int eligio = false;
     int lectura;
     while (!eligio) {
       lectura = ultrasonic_7.distanceCm();
-      if (lectura < 20) {
+      if (lectura < 30) {
         eligio = true;
-        if (lectura < 10) {
-          return 1;
+        if (lectura < 20) {
+          if (lectura < 10) {
+            return 1;
+          }
+          return 2;
         }
-        return 2;
+        return 3;
       }
       _loop();
     }
@@ -166,6 +196,24 @@ void programa2() {
   }
   Serial.println("termino ejecucion");
 }
+
+void recorridoPoligono(){
+/* cantidad de lados del polígono = distancia al sensor ultrasónico a la que
+se pone la mano dividido 10cm */
+    int lectura = 400;
+    int lados;
+    while (lectura > 55) {
+      lectura = ultrasonic_7.distanceCm();
+    }
+    lados = lectura / 10;
+    setSpeed(normalSpeed);
+    _delay(1);
+    setSpeed(0);
+    _delay(1);
+    rotateLeft(((360) / (lados)) * (0.8333));
+    _delay(1);
+    _loop();
+}
 /* FIN FUNCIONES NUESTRAS */
 
 void loop(){
@@ -173,12 +221,16 @@ void loop(){
   Serial.println("inicio loop ppal");
   switch (leerOpcion()) {
     case 1:
-      Serial.println("entro a programa 1");
+      Serial.println("entro a programa 1: robot que se para cuando tiene algo adelante");
       robotQueSeParaCuandoTieneAlgoAdelante();
       break;
     case 2:
-      Serial.println("entro a programa 2");
+      Serial.println("entro a programa 2: giro un poquito las ruedas");
       programa2();
+      break;
+    case 3:
+      Serial.println("entro a programa 3: recorrido poligono");
+      recorridoPoligono();
       break;
     default:
       // do something
@@ -197,4 +249,5 @@ void _delay(float seconds){
 void _loop(){
     Encoder_1.loop();
     Encoder_2.loop();
+    gyro_1.update();
 }
